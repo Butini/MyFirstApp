65628e1f2babae4819151bd84028cfead4cf56de
Butini <marcbiedmabutier@gmail.com>
2021-06-25 13:48:21 +0200

    foreach implementation with arraylist example. Closes #1

---
+++ MyFirstApp/ForeachLoop/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.ForeachLoop
+{
+    public class Student
+    {
+        //Propiedades automaticas
+        public int StudentId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Surname { get; set; }
+
+        public int Age { get; set; }
+    }
+}
